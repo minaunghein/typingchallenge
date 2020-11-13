7ee4f41d4ea68f88c01e4f7d7bff70282ff346bb
Min Aung Hein <minaunghein@gmail.com>
2020-11-13 20:15:57 +0630

    first commit

---
+++ typing/Models/TypeingCategory.swift
@@ -0,0 +1,9 @@
+//
+//  TypeCategory.swift
+//  typing
+//
+//  Created by Min Aung Hein on 11/13/20.
+//  Copyright © 2020 Min Aung Hein. All rights reserved.
+//
+
+import Foundation
---
+++ typing/Models/Word.swift
@@ -0,0 +1,9 @@
+//
+//  Word.swift
+//  typing
+//
+//  Created by Min Aung Hein on 11/13/20.
+//  Copyright © 2020 Min Aung Hein. All rights reserved.
+//
+
+import Foundation
